05ecb92433f36ef413f0e6335c06fb4fe996a105
David <chaosdave34@users.noreply.github.com>
2024-03-04 19:34:33 +0100

    Added info message on server switch

--- src/main/java/net/gamerhub/rememberme/RememberMe.java
+++ src/main/java/net/gamerhub/rememberme/RememberMe.java
@@ -1,17 +1,25 @@
 package net.gamerhub.rememberme;
 
 import com.google.inject.Inject;
-import com.velocitypowered.api.event.player.PlayerChooseInitialServerEvent;
-import com.velocitypowered.api.event.player.ServerConnectedEvent;
+import com.velocitypowered.api.event.permission.PermissionsSetupEvent;
+import com.velocitypowered.api.event.player.*;
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
 import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.event.proxy.ProxyPingEvent;
+import com.velocitypowered.api.event.query.ProxyQueryEvent;
 import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.ProxyServer;
 import com.velocitypowered.api.proxy.server.RegisteredServer;
+import com.velocitypowered.api.proxy.server.ServerPing;
+import jdk.jfr.Registered;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
 import org.slf4j.Logger;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.UUID;
+import java.security.Permission;
+import java.util.*;
+import java.util.concurrent.CompletableFuture;
 
 @Plugin(
         id = "rememberme",
@@ -21,10 +29,15 @@ import java.util.UUID;
         authors = {"Chaosdave34"}
 )
 public class RememberMe {
+    private final ProxyServer server;
+    private final Logger logger;
     private static final Map<UUID, RegisteredServer> lastServer = new HashMap<>();
 
     @Inject
-    private Logger logger;
+    public RememberMe(ProxyServer server, Logger logger) {
+        this.server = server;
+        this.logger = logger;
+    }
 
     @Subscribe
     public void onProxyInitialization(ProxyInitializeEvent event) {
@@ -32,7 +45,10 @@ public class RememberMe {
 
     @Subscribe
     public void onPlayerSwitchServer(ServerConnectedEvent e) {
-        lastServer.put(e.getPlayer().getUniqueId(), e.getServer());
+        Player p = e.getPlayer();
+        RegisteredServer server = e.getServer();
+        lastServer.put(p.getUniqueId(), server);
+        p.sendMessage(Component.text("[Velocity] Sending you to server " + server.getServerInfo().getName() + "...", NamedTextColor.GRAY));
     }
 
     @Subscribe
