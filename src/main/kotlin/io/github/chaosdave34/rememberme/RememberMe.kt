a87cc7c91b73384307fb5e15bf451f45fe3134df
Chaosdave34 <david.emanuel@gmx.de>
2024-11-06 18:58:43 +0100

    Updated to 3.4.0-SNAPSHOT and ported to kotlin

--- src/main/kotlin/io/github/chaosdave34/rememberme/RememberMe.kt
+++ src/main/kotlin/io/github/chaosdave34/rememberme/RememberMe.kt
@@ -1,17 +1,14 @@
 package io.github.chaosdave34.rememberme
 
-import com.google.inject.Inject
 import com.velocitypowered.api.event.Subscribe
 import com.velocitypowered.api.event.player.PlayerChooseInitialServerEvent
 import com.velocitypowered.api.event.player.ServerConnectedEvent
 import com.velocitypowered.api.event.proxy.ProxyInitializeEvent
 import com.velocitypowered.api.plugin.Plugin
-import com.velocitypowered.api.proxy.ProxyServer
 import com.velocitypowered.api.proxy.server.RegisteredServer
 import net.gamerhub.rememberme.BuildConstants
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.text.format.NamedTextColor
-import org.slf4j.Logger
 import java.util.UUID
 
 @Suppress("unused")
@@ -22,7 +19,7 @@ import java.util.UUID
     description = "Remembers the last server you joined",
     authors = ["Chaosdave34"]
 )
-class RememberMe{
+class RememberMe {
     private val lastServer = mutableMapOf<UUID, RegisteredServer>()
 
     @Subscribe
